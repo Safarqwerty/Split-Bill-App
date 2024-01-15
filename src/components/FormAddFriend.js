fa43bf19f89c7e4137d16a3d9b03ed92d6808d61
Safarqwerty <safar.alam79@gmail.com>
2024-01-15 10:01:29 +0800

    Event Handler Untuk Membuat Data Object Teman

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -4,15 +4,33 @@ export default function FormAddFriend( onAddFriend ) {
     const [name, setName] = useState("");
     const [foto, setFoto] = useState("https://i.pravatar.cc/48");
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if(!name || !foto) return
+
+        const id = crypto.randomUUID();
+        const newFriend = {
+            id,
+            name,
+            foto: `${foto}?=${id}`,
+            balance: 0
+        }
+
+        onAddFriend(newFriend)
+        setName("")
+        setFoto("")
+    }
+
     return (
-        <form action="" className="form-add-friend">
+        <form action="" className="form-add-friend" onSubmit={handleSubmit}>
             <label htmlFor="">Nama</label>
             <input 
                 type="text" 
                 value={name} 
                 onChange={(e) => setName(e.target.value)}/>
             <label htmlFor="">Foto</label>
-            
+
             <input 
                 type="text" 
                 value={foto} 
