fd182b3105adfd19cbbfcc93b8d74a4e9b5f7c29
Safarqwerty <safar.alam79@gmail.com>
2024-01-17 01:41:01 +0800

    Membuat Logic Patungan

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,16 +1,26 @@
+import { useState } from "react"
+
 export default function FormSplitBill ({ selectedFriend }) {
+    const [amount, setAmount] = useState("")
+    const [myBill, setMyBill] = useState("")
+    const friendBill = amount ? amount - myBill : "";
+    const [whoIsPaying, setWhoIsPaying] = useState("user");
+
     return (
         <form action="" className="form-split-bill">
             <h2>Patungan dengan {selectedFriend.name}</h2>
 
             <label htmlFor="">Total Tagihan</label>
-            <input type="text" name="" id="" />
+            <input 
+                type="text"
+                value={amount} 
+                onChang={(e) => setAmount(e.target.value)}/>
             <label htmlFor="">Total Anda</label>
-            <input type="text" name="" id="" />
+            <input type="text" value={myBill} onChang={(e) => setMyBill(e.target.value)}/>
             <label htmlFor="">Tagihan {selectedFriend.name}</label>
-            <input type="text" name="" id="" disabled/>
+            <input type="text" value={friendBill} disabled/>
             <label htmlFor="">Dibayar Oleh</label>
-            <select name="" id="">
+            <select value={whoIsPaying} onChang={(e) => setWhoIsPaying(e.target.value)}>
                 <option value="user">Saya</option>
                 <option value="friend">{selectedFriend.name}</option>
             </select>
